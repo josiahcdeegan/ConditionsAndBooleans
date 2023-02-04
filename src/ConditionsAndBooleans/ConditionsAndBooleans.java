40ca196b87eda6f8eea966c8565f65e5b493ad5e
josiahcdeegan <114314800+josiahcdeegan@users.noreply.github.com>
2023-02-04 12:59:53 -0500

    Add files via upload

---
+++ src/ConditionsAndBooleans/ConditionsAndBooleans.java
@@ -0,0 +1,16 @@
+package ConditionsAndBooleans;
+import java.util.Scanner;
+
+public class ConditionsAndBooleans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 14; 
+		int b = 27; 
+		int c = -3; 
+		
+		boolean compare = a < b && c < a;
+		System.out.println(compare);
+	}
+
+}
